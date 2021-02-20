8e8b1d3b2b006e68886c56b6bc264d6daefc1e11
j985chen <j985chen@uwaterloo.ca>
2021-02-19 23:23:18 -0500

    removed the console.log debugging statements

--- commands/wiki.js
+++ commands/wiki.js
@@ -13,13 +13,11 @@ module.exports = {
             return message.channel.send("Error: search query must be non-empty");
         }
         const phrase = encodeURIComponent(args.join(" "));
-        console.log(phrase);
         request.get(process.env.WIKI_SEARCH_URL + "&search=" + phrase, { json: true }, (err, res, body) => {
             if (err) {
                 return console.log(err);
             }
             wiki_embed.setTitle(`Wikipedia search results for ${phrase}`);
-            console.log(body);
             if (body[1]?.length == 0) {
                 return message.channel.send(`No Wikipedia results found for ${phrase}`);
             }
@@ -29,7 +27,6 @@ module.exports = {
                 if (err) {
                     return console.log(err);
                 }
-                console.log(body.query.pages);
                 const results = body.query.pages;
                 if (Object.keys(results).length == 0) {
                     return message.channel.send(`No Wikipedia results found for ${phrase}`);
