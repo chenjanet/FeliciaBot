ce652fee5de4b7a4a567d12d5f957514ed73bd45
j985chen <j985chen@uwaterloo.ca>
2021-02-19 22:15:41 -0500

    added wikipedia search functionality

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const request = require('request');
 
 module.exports = {
     name: 'wiki',
@@ -11,6 +12,22 @@ module.exports = {
         if (args.length == 0) {
             return message.channel.send("Error: search query must be non-empty");
         }
-        
+        const phrase = encodeURIComponent(args.join(" "));
+        console.log(phrase);
+        request.get(process.env.WIKI_URL + "&titles=" + phrase, { json: true }, (err, res, body) => {
+            if (err) {
+                return console.log(err);
+            }
+            wiki_embed.setTitle(`Wikipedia search results for ${phrase}`);
+            console.log(res.statusCode);
+            const results = body.query.pages;
+            console.log(results);
+            for (let key in results) {
+                wiki_embed.addField(
+                    results[key].title, results[key].extract, false
+                );
+            }
+            return message.channel.send(wiki_embed);
+        });
     }
 }
--- helpers/apis.js
+++ helpers/apis.js
@@ -1,7 +1,6 @@
 const request = require('request');
 
 module.exports.getRequest = async(url, callback) => {
-    console.log(url);
     request.get(url, { json: true }, async (err, res, body) => {
         if (err) {
             return console.log(err);
