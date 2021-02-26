d1504374237ec9772ffd64662226cd09c2e23516
j985chen <j985chen@uwaterloo.ca>
2021-02-25 23:21:33 -0500

    removed tutorial commands beep/boop and ping/pong

--- commands/beep.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'beep',
-    description: 'responds with \'Boop!\' whenever `~beep` is typed',
-    args: false,
-    execute(message, args) {
-        message.channel.send('Boop!');
-    },
-};
--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'responds with \'Pong!\' whenever `~ping` is typed',
-    args: false,
-    execute(message, args) {
-        message.channel.send('Pong!');
-    },
-};
